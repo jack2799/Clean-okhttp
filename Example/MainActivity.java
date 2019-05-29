12d4c17b08c7c2f5c9610dfe5b91a32ff0d4489f
Shreyans Jain <jack2799@gmail.com>
2019-05-29 12:44:47 +0530

    Add files via upload

--- Example/MainActivity.java
+++ Example/MainActivity.java
@@ -1,21 +1,29 @@
-package jain.shreyans.cleanokhttp;
+package jain.shreyans.clean_okhttp;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.Toast;
 
+import jain.shreyans.clean_okhttp.*;
+import jain.shreyans.cleanokhttp.OkReq;
+import jain.shreyans.cleanokhttp.ResponseCallback;
+
 public class MainActivity extends AppCompatActivity implements ResponseCallback {
+
+    private static final int GET_FIRST_DATA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         OkReq request = new OkReq(this,this);
-        request.makeRequest("https://jsonplaceholder.typicode.com/todos/1",1);
+        request.makeRequest("https://jsonplaceholder.typicode.com/todos/1",GET_FIRST_DATA);
     }
 
     @Override
     public void onSuccess(String response, int requestCode) {
-        Toast.makeText(this,response,Toast.LENGTH_LONG).show();
+        if (requestCode==GET_FIRST_DATA) {
+            Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+        }
     }
 }
